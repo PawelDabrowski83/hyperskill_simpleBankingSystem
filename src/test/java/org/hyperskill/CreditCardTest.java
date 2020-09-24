183a1a838c5c633355c2a140075801f0e61db1d4
fluor <pawel.d.dabrowski@gmail.com>
2020-09-24 13:49:34 +0200

    CreditCard.validatePinNumber with tests

--- src/test/java/org/hyperskill/CreditCardTest.java
+++ src/test/java/org/hyperskill/CreditCardTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CreditCardTest {
 
@@ -43,10 +43,8 @@ public class CreditCardTest {
 
         // when
         String[] creditCardNumbers = new String[100];
-        int i = 0;
-        while (i < creditCardNumbers.length){
+        for (int i = 0; i < creditCardNumbers.length; i++){
             creditCardNumbers[i] = CreditCard.createCreditCardNumber();
-            i++;
         }
 
         // then
@@ -55,7 +53,46 @@ public class CreditCardTest {
             assertEquals(requiredLength, actualCreditCardNumber.length());
             assertTrue(CreditCard.CREDIT_CARD_NUMBER_FORMULA.matcher(actualCreditCardNumber).matches());
         }
+    }
 
+    @Test
+    public void shouldCreatePinNumberReturnValidPinNumber(){
+        // given
+        final int requiredLength = CreditCard.PIN_NUMBER_LENGTH;
 
+        // when
+        String[] pinNumbers = new String[100];
+        for (int i = 0; i < pinNumbers.length; i++){
+            pinNumbers[i] = CreditCard.createPinNumber();
+        }
+
+        // then
+        for (String actualPinNumber : pinNumbers){
+            assertEquals(CreditCard.PIN_NUMBER_LENGTH, actualPinNumber.length());
+            assertTrue(CreditCard.PIN_NUMBER_FORMULA.matcher(actualPinNumber).matches());
+        }
+    }
+
+    @DisplayName("should validatePinNumber() work with valid input")
+    @ParameterizedTest
+    @ValueSource(strings = {"1234", "0000", "9999", "7893", "1463", "1000", "4081"})
+    void validatePinNumberIsTrue(String given){
+        assertTrue(CreditCard.validatePinNumber(given));
+    }
+
+    @DisplayName("should validatePinNumber() returns false with invalid input")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  ", "123", "00", "12345", "kot", "test", "74289423", "1234;", "-100"})
+    void validatePinNumberIsFalse(String given){
+        assertFalse(CreditCard.validatePinNumber(given));
+    }
+
+    @Test
+    public void shouldValidatePinNumberGivenNullReturnFalse(){
+        // when
+        String given = null;
+
+        // then
+        assertFalse(CreditCard.validatePinNumber(null));
     }
 }
