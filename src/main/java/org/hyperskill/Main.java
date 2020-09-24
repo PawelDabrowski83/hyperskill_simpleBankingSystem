ba38466ec55a4462892f7d5bde746b66dbbd7cfd
fluor <pawel.d.dabrowski@gmail.com>
2020-09-24 16:24:24 +0200

    creating different package for various actions

--- src/main/java/org/hyperskill/Main.java
+++ src/main/java/org/hyperskill/Main.java
@@ -1,62 +1,14 @@
 package org.hyperskill;
 
-import org.hyperskill.menu.AccountMenuOptions;
-import org.hyperskill.menu.MainMenuOptions;
-
 import java.util.Scanner;
-import static org.hyperskill.menu.MainMenuOptions.*;
 
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)){
-            displayMainMenu(scanner);
+            UserConsole.displayMainMenu(scanner);
         }
     }
 
-    private static void displayMainMenu(Scanner scanner){
-        String command = "";
-        while (!EXIT.getValue().equals(command)){
-            MainMenuOptions option = getByValue(command);
-            switch (option){
-                case LOGIN -> manageUser(scanner);
-                case CREATE_ACCOUNT -> createAccount(scanner);
-            }
-            System.out.println(UserConsole.CONSOLE_MENU);
-            command = scanner.nextLine();
-        }
-        quitConsole();
-    }
 
-    private static void quitConsole(){
-        System.out.println(UserConsole.MENU_QUIT);
-        System.exit(0);
-    }
-
-    protected static void manageUser(Scanner scanner){
-        String command = "";
-        while (!EXIT.getValue().equals(command)){
-            AccountMenuOptions option = AccountMenuOptions.getByValue(command);
-            switch (option){
-                case BALANCE -> checkBalance(scanner);
-                case LOGOUT -> logout(scanner);
-            }
-            System.out.println(UserConsole.MENU_USER_ACCOUNT);
-            command = scanner.nextLine();
-        }
-        quitConsole();
-
-    }
-
-    protected static void createAccount(Scanner scanner){
-        System.out.println("create account");
-    }
-
-    protected static void checkBalance(Scanner scanner){
-        System.out.println("check balance");
-    }
-
-    protected static void logout(Scanner scanner){
-        System.out.println("logout");
-    }
 
 }
--- src/main/java/org/hyperskill/UserConsole.java
+++ src/main/java/org/hyperskill/UserConsole.java
@@ -1,5 +1,14 @@
 package org.hyperskill;
 
+import org.hyperskill.actions.QuitConsole;
+import org.hyperskill.menu.AccountMenuOptions;
+import org.hyperskill.menu.MainMenuOptions;
+
+import java.util.Scanner;
+
+import static org.hyperskill.menu.MainMenuOptions.EXIT;
+import static org.hyperskill.menu.MainMenuOptions.getByValue;
+
 public class UserConsole {
     protected static final String CONSOLE_MENU = "1. Create an account\n2. Log into account\n0. Exit";
     protected static final String MENU_CARD_CREATED = "Your card has been created";
@@ -12,7 +21,50 @@ public class UserConsole {
     protected static final String MENU_USER_ACCOUNT = "1. Balance\n2. Log out\n0. Exit";
     protected static final String MENU_LOGOUT_EXIT = "You have successfully logged out!";
     protected static final String MENU_BALANCE = "Balance: %d";
-    protected static final String MENU_QUIT = "Bye!";
+
+
+    protected static void displayMainMenu(Scanner scanner){
+        String command = "";
+        while (!EXIT.getValue().equals(command)){
+            MainMenuOptions option = getByValue(command);
+            switch (option){
+                case LOGIN -> manageUser(scanner);
+                case CREATE_ACCOUNT -> createAccount(scanner);
+            }
+            System.out.println(CONSOLE_MENU);
+            command = scanner.nextLine();
+        }
+        QuitConsole.exit();
+    }
+
+
+
+    protected static void manageUser(Scanner scanner){
+        String command = "";
+        while (!EXIT.getValue().equals(command)){
+            AccountMenuOptions option = AccountMenuOptions.getByValue(command);
+            switch (option){
+                case BALANCE -> checkBalance(scanner);
+                case LOGOUT -> logout(scanner);
+            }
+            System.out.println(MENU_USER_ACCOUNT);
+            command = scanner.nextLine();
+        }
+        QuitConsole.exit();
+
+    }
+
+    protected static void createAccount(Scanner scanner){
+        System.out.println("create account");
+    }
+
+    protected static void checkBalance(Scanner scanner){
+        System.out.println("check balance");
+    }
+
+    protected static void logout(Scanner scanner){
+        System.out.println("logout");
+    }
 }
 
 
